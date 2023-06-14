d31ac34b9abbeb9a3c57a700990f59a896ef7b36
samirjouni <lb.sam@icloud.com>
2023-06-14 04:02:25 +0200

    solved aufgabe5

--- Frontend/JS Debugging/aufgabe5.js
+++ Frontend/JS Debugging/aufgabe5.js
@@ -1,8 +1,8 @@
 function checkPalindrome(word) { var reverseWord = "";
-for (var i = 0; i >= 0; i--) {
+for (var i = word.length -1; i >= 0; i--) {
 reverseWord += word[i]; }
-if (reverseWord === word) { return false;
-} else { return true;
+if (reverseWord === word) { return true;
+} else { return false;
 } }
 var word = "level";
 var isPalindrome = checkPalindrome(word); console.log("Ist das Wort ein Palindrom? " + isPalindrome);
